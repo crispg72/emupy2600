42ad5bd7944cb2dcddb5a98c9046af58a781846e
crispg72 <crispg72@users.noreply.github.com>
2017-12-04 21:16:24 +0000

    First simple test

---
+++ tests/test_tia.py
@@ -0,0 +1,15 @@
+import pytest
+from emupy2600.tia import Tia
+
+
+def test_set_single_line_colour():
+	tia = Tia()
+
+	# Set some crazy values
+	tia.current_scan_line = 1001
+	tia.current_line_clk = 999
+
+	# write 0 to VBLANK (0x01)
+	tia.write(0x01, 0)
+	assert tia.current_scan_line == 0
+	assert tia.current_line_clk == 0
