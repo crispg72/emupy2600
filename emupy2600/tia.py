2ef99581a84a1c96c6a7ece187db650665be08b3
chris <chris@debianVM.madeup>
2017-12-04 20:13:01 +0000

    Some more init

--- emupy2600/tia.py
+++ emupy2600/tia.py
@@ -2,8 +2,20 @@
 
 class tia(object):
 
-	def __init__(self):
+	def __init__(
+		self, mode = 'PAL',
+		display_driver = None):
+
+        if mode == "PAL":
+            self.total_scan_lines = 312
+            self.clocks_per_line = 228
+        else:
+            self.total_scan_lines = 262
+            self.clocks_per_line = 228
+
 		self.background_colour = 0
+		self.current_scan_line = 0
+		self.current_line_clk = 0
 		self.write_fn = {
 			0x02: self.write_wsync
 			0x09: self.write_colubk
