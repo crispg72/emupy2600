39eceade3462197096fb9091f7a7730b52c494fa
crispg72 <crispg72@users.noreply.github.com>
2017-12-24 13:16:08 +0000

    read and write

--- emupy2600/memory.py
+++ emupy2600/memory.py
@@ -2,13 +2,25 @@
 
 class Memory(object):
 
-	def __init__(self, size, rom = None):
+    def __init__(self, size, tia, rom = None):
 
-		if rom:
-			org = rom[0] + (rom[1] << 8)
-			self.buffer = bytearray(size + org)
-			for b in range(2, size):
-				self.buffer[org] = rom[b]
-				org += 1
-		else:
-			self.buffer = bytearray(size)
+        if rom:
+            org = rom[0] + (rom[1] << 8)
+            self.buffer = bytearray(size + org)
+            for b in range(2, size):
+                self.buffer[org] = rom[b]
+                org += 1
+        else:
+            self.buffer = bytearray(size)
+
+        self.tia = tia
+
+
+    def read(self, address):
+        return self.buffer[address]
+
+    def write(self, address, value):
+        if address < 0x80:
+            self.tia.write(address, value)
+        else:
+            self.buffer[address] = value
