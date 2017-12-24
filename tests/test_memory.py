8e050f06fa30fb5e6e25e2351a15c5750be3a3b7
crispg72 <crispg72@users.noreply.github.com>
2017-12-24 13:27:31 +0000

    Fixed failing  test_load_rom

--- tests/test_memory.py
+++ tests/test_memory.py
@@ -5,6 +5,6 @@ def test_load_rom():
 	sequence = bytearray(b'\x01\x03\x05\x04\x02')
 	test_rom = bytearray(b'\x08\x00\x00\x00\x00\x00\x00\x00\x00\x00')
 	test_rom += sequence
-	mem = Memory(len(test_rom), test_rom)
+	mem = Memory(len(test_rom), None, test_rom)
 
 	assert mem.buffer[16:16+len(sequence)] == sequence
