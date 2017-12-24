14a229386b6fa30764f16bd5a8f83fe5d2580cdc
crispg72 <crispg72@users.noreply.github.com>
2017-12-24 13:15:43 +0000

    respects colour, sizes

--- emupy2600/displaydriver_pygame.py
+++ emupy2600/displaydriver_pygame.py
@@ -3,22 +3,22 @@ import pygame
 
 class DisplayDriverPyGame(object):
 
-	def __init__(self, width, height):
+    def __init__(self, width, height):
 
-        self.size = width, height 
+        self.size = (width, height)
         self.screen = pygame.display.set_mode(self.size)
 
 
-	def draw_horizontal_line(
-			self, xstart, xend, y):
+    def draw_horizontal_line(
+            self, xstart, xend, y, colour):
 
-		pygame.draw.line(
+        pygame.draw.line(
                 pygame.display.get_surface(), 
-                (0,127,0), 
-                (0,self.current_scanline), 
-                (self.size.width, 200))
+                colour, 
+                (xstart,y), 
+                (xend - xstart, y))
 
 
-	def end_frame(self):
+    def end_frame(self):
         pygame.display.flip()
 
