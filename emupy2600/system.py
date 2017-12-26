424f2e05d0e58b5c368a9ed8a739a8842ad44101
crispg72 <crispg72@users.noreply.github.com>
2017-12-26 16:12:19 +0000

    Tia accepts cpu

--- emupy2600/system.py
+++ emupy2600/system.py
@@ -8,7 +8,11 @@ class System(object):
 	def __init__(self, cpu, rom, display_driver):
 		self.cpu = cpu
 		self.cpu.registers.pc = rom[0] + (rom[1] << 8)
-		self.tia = Tia(mode = 'NTSC', display_driver = display_driver)
+		self.tia = Tia(
+			mode = 'NTSC', 
+			display_driver = display_driver, 
+			cpu = self.cpu
+		)
 		self.memory = Memory(len(rom), self.tia, rom)
 		self.cpu.memory_controller = self.memory
 
